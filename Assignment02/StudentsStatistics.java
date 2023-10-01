f7a120cefe8eac5624ea335063ed2952bb0a2436
Kulana Avinash <kulanaavinash2018@gmail.com>
2023-10-01 23:30:04 +1100

    read data from csv

--- Assignment02/StudentsStatistics.java
+++ Assignment02/StudentsStatistics.java
@@ -1,14 +1,43 @@
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
 /**
- * Write a description of class StudentsStatistics here.
+ * This is a program to compute statistics of students marks in an assignment.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Kulana Avinash Welihena Vithanage)
+ * @version (version 1.0 28/09/2023)
  */
 public class StudentsStatistics
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    public static void main(String[] args) {
+        String csvFile = "prog5001_students_grade_2022.csv";
+        String line;
+        String delimiter = ",";
+        List<Student> students = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            br.readLine();
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(delimiter);
+                String lastName = data[0];
+                String firstName = data[1];
+                String studentID = data[2];
+                int a1 = Integer.parseInt(data[3]);
+                int a2 = Integer.parseInt(data[4]);
+                int a3 = Integer.parseInt(data[5]);
+                Student student = new Student(lastName, firstName, studentID, a1, a2, a3);
+                students.add(student);
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Constructor for objects of class StudentsStatistics
@@ -16,18 +45,8 @@ public class StudentsStatistics
     public StudentsStatistics()
     {
         // initialise instance variables
-        x = 0;
+
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+   
 }
