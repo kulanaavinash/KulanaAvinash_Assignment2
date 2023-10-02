4d8ae9e46b21316c72ab5ef95f3ece4e2402db9e
Kulana Avinash <kulanaavinash2018@gmail.com>
2023-10-02 18:22:42 +1100

    implement project structure

---
+++ Assignment02/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+/**
+ * This is a program to compute statistics of students marks in an assignment.
+ *
+ * @author (Kulana Avinash Welihena Vithanage)
+ * @version (version 1.0 28/09/2023)
+ */
+class Student
+{
+
+}
+class StudentsStatistics
+{
+
+}
+
+
+public class Main
+{
+    public static void main(String[] args) {
+
+    }
+
+}
